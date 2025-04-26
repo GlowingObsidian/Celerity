fe39dd6cb6ddc722a1f40c04f69d84308c1d4859
Shreyan Dey <deyshreyan24@outlook.com>
2025-04-26 15:20:15 +0530

    Satisying build requirement

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -41,7 +41,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 type OperationStatus = "IDLE" | "PROCESSING" | "SUCCESS" | "ERROR";
 
